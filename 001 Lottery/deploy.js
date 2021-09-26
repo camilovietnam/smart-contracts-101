8be14f76b377cb71ef57a24f2951266d6ab8c222
Camilo <camilovietnam@gmail.com>
2021-09-26 13:09:12 +0700

    remove course key

--- 001 Lottery/deploy.js
+++ 001 Lottery/deploy.js
@@ -3,7 +3,7 @@ const Web3 = require("web3")
 const { interface, bytecode } = require("./compile")
 require('dotenv').config()
 
-// bulk glue jewel country lottery remember extra shadow donor patient symptom tumble
+
 const provider = new HDWalletProvider(
      process.env.WALLET_SECRET,
     'https://rinkeby.infura.io/v3/283b88436bcc4577af31a2f2ac1493e6'
